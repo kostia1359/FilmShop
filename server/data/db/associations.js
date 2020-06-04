955f5d0ff472497f9da1aed0c2cdd9b8724c3f7b
kostia1359 <tzimbal.k@gmail.com>
2020-06-04 14:35:27 +0300

    changed relation between films and awards from n to n to 1 to n

--- server/data/db/associations.js
+++ server/data/db/associations.js
@@ -2,14 +2,14 @@ function setAssociations(models) {
     const {Film, Genre, Award, Description} = models;
 
     Film.belongsToMany(Genre, {through: 'FilmGenres'});
-    Film.belongsToMany(Award, {through: 'FilmAwards'});
+    Film.hasMany(Award);
     Film.belongsTo(Description);
 
     Genre.belongsToMany(Film, {through: 'FilmGenres'});
 
     Description.hasOne(Film);
 
-    Award.belongsToMany(Film, {through: 'FilmAwards'});
+    Award.belongsTo(Film);
 }
 
 module.exports=setAssociations;
