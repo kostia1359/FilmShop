2e4fb8cb772e30c9bfe2371aa9eb57d602d318f8
kostia1359 <tzimbal.k@gmail.com>
2020-06-19 09:12:54 +0300

    add random views

--- server/api/routes/contentRoutes.js
+++ server/api/routes/contentRoutes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const mustAuthenticated=require('../../helpers/isAuthorized');
+const request = require('request');
+
 
 const router = Router();
 const clientDirectory=__dirname.replace('\\server\\api\\routes','\\client\\');
@@ -15,8 +17,22 @@ router.get('/tables',mustAuthenticated, function (request,response) {
 })
 
 router.post('/tables', mustAuthenticated,function (request, response) {
-    console.log('123');
+
     response.redirect('/content/tables');
+})
+
+router.get('/filmImage/:year/:filmName',async function (req,response) {
+    const year=Number(req.params.year);
+    const filmName = req.params.filmName;
+    let img;
+    await request(`https://www.joblo.com/movie-posters/${year}/${filmName}`, function(err, res, body) {
+        const lastIndex=body.indexOf('<a class="poster-image-ralated"');
+        const srcIndex=body.indexOf('<img src="',lastIndex)+10;
+        const lastSrcIndex=body.indexOf('"',srcIndex);
+
+        img='https://www.joblo.com'+body.slice(srcIndex,lastSrcIndex);
+        response.status(200).send({img});
+    });
 })
 
 router.get('/additional',function (request,response) {
